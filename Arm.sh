31892d3c8aedaab36677fed7a91b91f97a09c0f0
Vikram <51349461+codexgegas@users.noreply.github.com>
2021-08-27 17:39:16 +0530

    Update Arm.sh

--- Arm.sh
+++ Arm.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
-echo  "Enter number: "
-read num
-for((i=2; i<=num/2; i++))
+echo "Enter a number: "
+read c
+
+x=$c
+sum=0
+r=0
+n=0
+while [ $x -gt 0 ]
 do
-  if [ $((num%i)) -eq 0 ]
-  then
-    echo "$num is not a prime number."
-    exit
-  fi
+r=`expr $x % 10`
+n=`expr $r \* $r \* $r`
+sum=`expr $sum + $n`
+x=`expr $x / 10`
 done
-echo "$num is a prime number."
+
+if [ $sum -eq $c ]
+then
+echo "It is an Armstrong Number."
+else
+echo "It is not an Armstrong Number."
+fi
